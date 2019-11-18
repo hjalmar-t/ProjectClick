5ae8b18aede4c07d1f0237b020dca4ea90067f34
hjalmar-t <jari_tervo@hotmail.com>
2019-11-18 13:28:28 +0200

    a0.02 Click-target registeration added for future use. Rudimentary UI for inventory management added. Item-class, not final.

---
+++ Project Click/Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemImage;
+
+    List<string> combineWith = new List<string>();
+    List<string> combineTo = new List<string>();
+
+    bool canCombine() {
+        if(combineWith[0] != null) return true;
+        return false;
+    }
+
+}
---
+++ Project Click/Assets/Scripts/MonoBehaviour/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject clickTarget;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.P) && clickTarget != null) Debug.Log("Currentmost clicked object: " + clickTarget.name);
+
+        Ray mousePointNormal = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit mouseClick;
+
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            if(Physics.Raycast(mousePointNormal, out mouseClick, Mathf.Infinity)) {
+                agent.destination = mouseClick.point;
+                clickTarget = mouseClick.collider.gameObject;
+            }
+        }
+    }
+}
